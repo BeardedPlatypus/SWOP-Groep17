9440f65e61e78ae738cedfeeedba3f46c4154565
mwtegelaers <mwtegelaers@gmail.com>
2014-03-05 21:26:03 +0100

    DateTime implementation.

--- src/domain/DateTime.java
+++ src/domain/DateTime.java
@@ -1,33 +1,166 @@
 package domain;
 
+/**
+ * The DateTime class is an immutable data object that contains the number of
+ * days, hours, and minutes.
+ * Created by the ProductionSchedule class. 
+ * 
+ * @author Martinus WilhelmusTegelaers
+ *
+ * @invariant 0 <= this.getDays()  
+ * @invariant 0 <= this.getHours() <= 23
+ * @invariant 0 <= this.getMinutes() <= 59
+ */
 public class DateTime {
-	public final int days;
-	public final int hours;
-	public final int minutes;
+	//--------------------------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------------------------
+	/**
+	 * Construct a new DateTime with the given days, hours, and minutes.
+	 * 
+	 * @param days
+	 * 		The number of days of this new DateTime.
+	 * @param hours
+	 * 		The number of hours of this new DateTime.
+	 * @param minutes
+	 * 		The number of minutes of this new DateTime. 
+	 * 
+	 * @postcondition | (new this).dateTime.getDays() == DateTime.sanitize(days, hours, minutes)[0]
+	 * @postcondition | (new this).dateTime.getHours() == DateTime.sanitize(days, hours, minutes)[1]
+	 * @postcondition | (new this).dateTime.getMinutes() == DateTime.sanitize(days, hours, minutes)[2]
+	 */
+	public DateTime(int days, int hours, int minutes) {
+		int[] new_values = DateTime.sanitise(days, hours, minutes);
+		
+		this.days = new_values[0];
+		this.hours = new_values[1];
+		this.minutes = new_values[2];
+	}
 	
+	/**
+	 * Sanitise the days, hours and minutes so that they confirm to the DateTime
+	 * invariants.
+	 * 
+	 * @param days
+	 * 		The input days.
+	 * @param hours
+	 * 		The input hours.
+	 * @param minutes
+	 * 		The input minutes.
+	 * 
+	 * @return {<sanitised days>, <sanitised hours>, <sanitised minutes>}
+	 */
+	public static int[] sanitise(int days, int hours, int minutes) {
+		long temp = days * 1440 + hours * 60 + minutes;
+		
+		if (temp <= 0)
+			return new int[]{0, 0, 0};
+		
+		int[] result = new int[3];
+		result[0] = (int) (temp / 1440);
+		result[1] = (int) ((temp % 1440) / 60);
+		result[2] = (int) (temp % 60);
+		return result;
+	}
+
+	//--------------------------------------------------------------------------
+	// Properties
+	//--------------------------------------------------------------------------
+	/** 
+	 * Get the days of this DayTime.
+	 * 
+	 * @return The days of this DayTime
+	 */
 	public int getDays() {
-		return days;
+		return this.days;
 	}
 
+	/** The days of this DateTime. */
+	public final int days;
+
+	/**
+	 * Get the hours of this DayTime
+	 * 
+	 * @return The hours of this DayTime
+	 */
 	public int getHours() {
-		return hours;
+		return this.hours;
 	}
 
+	/** The hours of this DateTime. */
+	public final int hours;
+	
+	/**
+	 * Get the minutes of this DateTime.
+	 * 
+	 * @return The minutes of this DateTime.
+	 */
 	public int getMinutes() {
-		return minutes;
+		return this.minutes;
+	}
+	
+	/** The minutes of this DateTime. */
+	public final int minutes;
+
+	//--------------------------------------------------------------------------
+	// Creation Methods. 
+	//--------------------------------------------------------------------------
+	/**
+	 * Create a new DateTime with the current DateTime plus the specified days,
+	 * hours, and minutes. 
+	 * 
+	 * @param days
+	 * 		The days to be added to this days. 
+	 * @param hours
+	 * 		The hours to be added to this hours. 
+	 * @param minutes
+	 * 		The minutes to be added this minutes.
+	 * 
+	 * @return new DateTime(days + this.getDays(), hours + this.getHours(), minutes + this.getMinutes())
+	 */
+	public DateTime addTime(int days, int hours, int minutes) {
+		return new DateTime(days + this.getDays(), hours + this.getHours(), minutes + this.getMinutes());
+	}
+	
+	/**
+	 * Create new DateTime with the current DateTime + the specified DateTime values
+	 * 
+	 * @param dt
+	 * 		The DateTime values to be added to this DateTime. 
+	 * 
+	 * @effect addTime(dt.days, dt.hours, dt.minutes)
+	 */
+	public DateTime addTime(DateTime dt) {
+		return this.addTime(dt.getDays(), dt.getHours(), dt.getMinutes());
 	}
 
-	public DateTime(int days, int hours, int minutes){
-		this.days = days;
-		this.hours = hours;
-		this.minutes = minutes;
+	//--------------------------------------------------------------------------
+	/**
+	 * Create a new DateTime with the current DateTime subtract the specified days,
+	 * hours, and minutes. 
+	 * 
+	 * @param days
+	 * 		The days to be added to this days. 
+	 * @param hours
+	 * 		The hours to be added to this hours. 
+	 * @param minutes
+	 * 		The minutes to be added this minutes.
+	 * 
+	 * @return new DateTime(days + this.getDays(), hours + this.getHours(), minutes + this.getMinutes())
+	 */
+	public DateTime subtractTime(int days, int hours, int minutes) {
+		return new DateTime(this.getDays() - days, this.getHours() - hours, this.getMinutes() - minutes);
 	}
-	
-	public DateTime progressTimeBy(int days, int hours, int minutes){
-		int newDays = this.days + days + ((this.hours + hours)/24);
-		int newHours = ((this.hours + hours) % 24) + ((this.minutes + minutes)/60);
-		int newMinutes = ((this.minutes + minutes) % 60);
-		return new DateTime(newDays, newHours, newMinutes);
+
+	/**
+	 * Create new DateTime with the current DateTime - the specified DateTime values
+	 * 
+	 * @param dt
+	 * 		The DateTime values to be added to this DateTime. 
+	 * 
+	 * @effect subtractTime(dt.days, dt.hours, dt.minutes)
+	 */
+	public DateTime subtractTime(DateTime dt) {
+		return this.subtractTime(dt.getDays(), dt.getHours(), dt.getMinutes());
 	}
-	
 }
--- test/domain/DateTimeTest.java
+++ test/domain/DateTimeTest.java
@@ -15,37 +15,54 @@ public class DateTimeTest {
 	}
 
 	@Test
+	public void test_constructorDateTime() throws Exception {
+		DateTime test1 = new DateTime(0, 0, 0);
+		
+		assertEquals(0, test1.days);
+		assertEquals(0, test1.hours);
+		assertEquals(0, test1.minutes);
+		
+		DateTime test2 = new DateTime(20, 5, 6);
+		
+		assertEquals(20, test2.days);
+		assertEquals(5, test2.hours);
+		assertEquals(6, test2.minutes);
+
+		DateTime test3 = new DateTime(51, 2, 0);
+		
+		assertEquals(51, test3.days);
+		assertEquals(2, test3.hours);
+		assertEquals(0, test3.minutes);
+
+	}
+	
+	@Test
+	public void test_constructorDateTimeNegativeInputIllegalArgument() {
+		
+	}
+	
+	@Test
 	public void progressTimeTest() {
 		DateTime currentDateTime = sample111DateTime;
 		assertEquals(currentDateTime.getDays(), 1);
 		assertEquals(currentDateTime.getHours(), 1);
 		assertEquals(currentDateTime.getMinutes(), 1);
-		currentDateTime = currentDateTime.progressTimeBy(3, 2, 4);
+		currentDateTime = currentDateTime.addTime(3, 2, 4);
 		assertEquals(currentDateTime.getDays(), 4);
 		assertEquals(currentDateTime.getHours(), 3);
 		assertEquals(currentDateTime.getMinutes(), 5);
-		currentDateTime = currentDateTime.progressTimeBy(0, 0, 59);
+		currentDateTime = currentDateTime.addTime(0, 0, 59);
 		assertEquals(currentDateTime.getDays(), 4);
 		assertEquals(currentDateTime.getHours(), 4);
 		assertEquals(currentDateTime.getMinutes(), 4);
-		currentDateTime = currentDateTime.progressTimeBy(0, 22, 58);
+		currentDateTime = currentDateTime.addTime(0, 22, 58);
 		assertEquals(currentDateTime.getDays(), 5);
 		assertEquals(currentDateTime.getHours(), 3);
 		assertEquals(currentDateTime.getMinutes(), 2);
-		currentDateTime = currentDateTime.progressTimeBy(16, 23, 59);
+		currentDateTime = currentDateTime.addTime(16, 23, 59);
 		assertEquals(currentDateTime.getDays(), 22);
 		assertEquals(currentDateTime.getHours(), 3);
 		assertEquals(currentDateTime.getMinutes(), 1);
-		currentDateTime = currentDateTime.progressTimeBy(-1, -1, -1);
-		assertEquals(currentDateTime.getDays(), 21);
-		assertEquals(currentDateTime.getHours(), 2);
-		assertEquals(currentDateTime.getMinutes(), 0);
-		currentDateTime = currentDateTime.progressTimeBy(0, 0, -1);
-		assertEquals(currentDateTime.getDays(), 21);
-		assertEquals(currentDateTime.getHours(), 1);
-		assertEquals(currentDateTime.getMinutes(), 59);
-		
-		
 	}
 
 }
