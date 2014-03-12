e55a9972d4ed49f721adced1ce0c358a11c11b73
SimSla <simon@Simons-MacBook-Air.local>
2014-03-13 00:53:07 +0100

    garageholder
    99% van de code. Input/output nog wat afscheiden + commentaar nodig.

--- src/interfaceLayer/GarageHolderTerminal.java
+++ src/interfaceLayer/GarageHolderTerminal.java
@@ -1,5 +1,8 @@
 package interfaceLayer;
 
+import java.io.IOException;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 import domain.*;
@@ -10,24 +13,236 @@ public class GarageHolderTerminal {
 		showHeader();
 		//TODO goeie contructor
 		NewOrderSessionHandler handler = null;
-		showOrderOverview(handler);
 		
+		while(true){
+			//Show overview
+			showOrderOverview(handler);
+			
+			if(userWantsOrder()){
+				Model model = selectModel(handler.getNewOrderModels());
+				Specification spec = composeOrder(model);
+				
+				if(userConfirmsOrder(model,spec)){
+					handler.chooseModelAndSpecifications(model, spec);
+					
+					//Easiest way to show estimated completion time at the moment...
+					List<OrderContainer> allOrders = new LinkedList<OrderContainer>();
+					allOrders.addAll(handler.getPendingOrders());
+					Collections.sort(allOrders, new OrderDatesDescendingComparator());
+					System.out.print("Order placed. Estimated completion time: ");
+					System.out.println(allOrders.get(0).getEstimatedCompletionTime().toString());
+					System.out.println();
+				}
+				
+				continue;
+			}
+			return;
+		}
+	}
+
+	private static boolean userConfirmsOrder(Model model, Specification spec) {
+		
+		System.out.println("Please review your choices.");
+		System.out.println("Model: " + model.getModelName());
+		
+		int numberOfOptions = model.getAmountOfOptions();
+		
+		for(int i = 0; i < numberOfOptions; i++){
+			Option option = model.getModelOption(i);
+			int choice = spec.getSpec(i);
+			System.out.println(option.getOptionName() + ": " + option.getChoiceName(choice));
+		}
+		
+		System.out.println();
+		System.out.println("Do you want to place this order?");
+		System.out.println("[Y]es");
+		System.out.println("[N]o");
+		System.out.println();
+		
+		try {
+			while(true){
+				// We'll only read in the first character.
+				String choice = String.valueOf((char) System.in.read());
+				// Don't forget to clear the input buffer.
+				System.in.skip(System.in.available());
+				if(choice.equalsIgnoreCase("Y")){
+					return true;
+				}else if (choice.equalsIgnoreCase("N")){
+					return false;
+				}else{
+					System.out.println();
+					System.out.println("Sorry, but \"" + choice + "\" is not a valid response. Please try again.");
+					System.out.println();
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("Critical error: Unable to read user input.");
+			e.printStackTrace();
+		}
+		
+		return false;
+	}
+
+	private static Specification composeOrder(Model model) {
+		
+		int amountOfOptions = model.getAmountOfOptions();
+		int[] selection = new int[amountOfOptions];
+		
+		for(int i = 0; i < amountOfOptions; i++){
+			
+			Option option = model.getModelOption(i);
+			System.out.println("Option " + (i+1) + ": " + option.getOptionName());
+			int amountOfChoices = option.getAmountOfChoices();
+			
+			for(int count = 0; count < amountOfChoices; count++){
+				System.out.println("[" + (count+1) + "] " + option.getChoiceName(count));
+			}
+			System.out.println();
+			
+			try{
+			out:while(true){
+					// We'll only read in the first character.
+					int choice = (int) System.in.read();
+					// Don't forget to clear the input buffer.
+					System.in.skip(System.in.available());
+					
+					if(choice < 1 || choice > amountOfChoices){
+						System.out.println();
+						System.out.println("Sorry, but \"" + choice + "\" is not a valid choice. Please try again.");
+						System.out.println();
+					}else{
+						selection[i] = choice;
+						break out;
+					}
+				}
+			} catch (IOException e) {
+				System.err.println("Critical error: Unable to read user input.");
+				e.printStackTrace();
+			}
+			
+			System.out.println();
+			
+		}
+		return model.makeSpecification(selection);
+		
+	}
+
+	private static Model selectModel(List<Model> newOrderModels) {
+		
+		System.out.println();
+		System.out.println("Please select a car model...");
+		for(int i = 0; i < newOrderModels.size(); i++){
+			System.out.println("[" + (i+1) + "] " + newOrderModels.get(i).getModelName());
+		}
+		
+		System.out.println();
+		try{
+			while(true){
+				// We'll only read in the first character.
+				int choice = (int) System.in.read();
+				// Don't forget to clear the input buffer.
+				System.in.skip(System.in.available());
+				
+				if(choice < 1 || choice > newOrderModels.size()){
+					System.out.println();
+					System.out.println("Sorry, but \"" + choice + "\" is not a valid option. Please try again.");
+					System.out.println();
+				}else{
+					return newOrderModels.get(choice-1);
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("Critical error: Unable to read user input.");
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+
+	private static boolean userWantsOrder() {
+		
+		System.out.println("Do you want to...");
+		System.out.println("1. [P]lace new order.");
+		System.out.println("2. [E]xit the overview.");
+		System.out.println();
+		
+		try {
+			while(true){
+				// We'll only read in the first character.
+				String choice = String.valueOf((char) System.in.read());
+				// Don't forget to clear the input buffer.
+				System.in.skip(System.in.available());
+				if(choice.equalsIgnoreCase("P")){
+					return true;
+				}else if (choice.equalsIgnoreCase("E")){
+					return false;
+				}else{
+					System.out.println();
+					System.out.println("Sorry, but \"" + choice + "\" is not a valid response. Please try again.");
+					System.out.println();
+				}
+			}
+		} catch (IOException e) {
+			System.err.println("Critical error: Unable to read user input.");
+			e.printStackTrace();
+		}
+		
+		return false;
 	}
 
 	private static void showOrderOverview(NewOrderSessionHandler handler) {
-		showCompletedOrders(handler);
-		showPendingOrders(handler);
-	}
-
-	private static void showCompletedOrders(NewOrderSessionHandler handler) {
-		List<OrderContainer> completedOrders = handler.getCompletedOrders();
-		OrderContainer o = completedOrders.get(0);
-		//erc
 		
+		System.out.println("ORDER OVERVIEW");
+		System.out.println("---------------");
+		System.out.println();
+		System.out.println("|- PENDING");
+		System.out.println();
+		
+		//pending
+		showOrders(handler.getPendingOrders());
+		
+		System.out.println("---------------");
+		System.out.println();
+		System.out.println("|- COMPLETED");
+		System.out.println();
+		
+		//completed
+		List<OrderContainer> allOrders = new LinkedList<OrderContainer>();
+		allOrders.addAll(handler.getCompletedOrders());
+		Collections.sort(allOrders, new OrderDatesDescendingComparator());
+		
+		showOrders(allOrders);
+		
+		System.out.println("---------------");
+		System.out.println();
 	}
 
-	private static void showPendingOrders(NewOrderSessionHandler handler) {
-		// TODO Auto-generated method stub
+	private static void showOrders(List<OrderContainer> completedOrders) {
+		
+		for(OrderContainer order : completedOrders){
+			Model model = order.getModel();
+			
+			System.out.println("Order number: " + order.getOrderNumber());
+			//TODO: or do we need creation date for completed orders?
+			if(order.isCompleted()) {
+				System.out.print("Completed at: ");
+			}else{
+				System.out.print("Est. completion: ");
+			}
+			System.out.println(order.getEstimatedCompletionTime().toString());
+			System.out.println("    Model: " + model.getModelName());
+			
+			Specification spec = order.getSpecifications();
+			for(int i = 0; i < spec.getAmountofSpecs(); i++){
+				int chosenOption = spec.getSpec(i);
+				Option option = model.getModelOption(i);
+				String optionDescriptor = option.getOptionName();
+				String chosenOptionName = option.getChoiceName(chosenOption);
+				System.out.println("    " + optionDescriptor + ": " + chosenOptionName);
+			}
+			
+			System.out.println();
+		}
 		
 	}
 
