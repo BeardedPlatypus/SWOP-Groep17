35efc3da2f25a34fd4fe73c431d49070c1ed26f5
BeardedPlatypus <mwtegelaers@gmail.com>
2014-04-13 16:41:20 +0200

    marked as todo.

--- src/domain/CompleteOrderCatalog.java
+++ src/domain/CompleteOrderCatalog.java
@@ -3,6 +3,7 @@ package domain;
 import java.util.ArrayList;
 import domain.Order;
 
+//TODO new in iteration 2
 public class CompleteOrderCatalog {
 	public ArrayList<Order> completedOrders = new ArrayList<Order>();
 
