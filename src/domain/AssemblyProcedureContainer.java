876889d775d779b0a6d5699dc5c56f7db74d8705
BeardedPlatypus <mwtegelaers@gmail.com>
2014-04-13 15:40:10 +0200

    Updated AssemblyProcedure with new code.

--- src/domain/AssemblyProcedureContainer.java
+++ src/domain/AssemblyProcedureContainer.java
@@ -1,8 +1,35 @@
 package domain;
 
+import java.util.List;
+
+/**
+ * AssemblyProcedureContainer is a view object that provides an immutable interface
+ * for accessing AssemblyProcedures. 
+ *
+ * @author Martinus Wilhelmus Tegelaers, Thomas Vochten
+ */
 public interface AssemblyProcedureContainer {
-
+	/**
+	 * Get the order fulfilled by this assembly procedure.
+	 * 
+	 * @return The OrderView of this AssemblyProcedure.
+	 */
 	public OrderContainer getOrder();
-
-	public AssemblyTaskContainer[] toAssemblyTasksArray();
+	
+	/**
+	 * Get the AssemblyTaskContainers of this AssemblyProcedure.
+	 * 
+	 * @return The AssemblyTaskContainers of this AssemblyProcedure.
+	 */
+	public List<AssemblyTaskContainer> getAssemblyTasks();
+	
+	/**
+	 * Get the AssemblyTaskContainers of this AssemblyProcedure that match the specified TaskType. 
+	 * 
+	 * @param taskType
+	 * 		The TaskType used to filter the AssemblyTasks. 
+	 * 
+	 * @return A list of AssemblyTaskContainers that match the specified TaskType
+	 */
+	public List<AssemblyTaskContainer> getAssemblyTasks(TaskType taskType);
 }
