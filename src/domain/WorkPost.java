d959528cced2744b50f921963da14b6a7e2aab6d
BeardedPlatypus <mwtegelaers@gmail.com>
2014-04-15 00:54:31 +0200

    Updated WorkPost and WorkPostContainer.

--- src/domain/WorkPost.java
+++ src/domain/WorkPost.java
@@ -1,37 +1,108 @@
 package domain;
 
+import java.util.List;
+
 /**
  * A class for workposts that are part of an assembly line.
  * A workpost has a type, and a name, derived from its type.
  * It also has a number, which depicts it's relative order in the assemblyline.
  */
 public class WorkPost implements WorkPostContainer {
-	private TaskType workPostType;
-	private AssemblyProcedure activeAssembly;
-
-	public void getTaskContainers() {
-		throw new UnsupportedOperationException();
+	//--------------------------------------------------------------------------
+	// Constructor
+	//--------------------------------------------------------------------------
+	/**
+	 * Initialise this new WorkPost with the specified TaskTyp and number. 
+	 * 
+	 * @param workPostType
+	 * 		The type of this new work post.
+	 * 
+	 * @post | (new this).getTaskType() == workPostType
+	 * @post | (new this).getPostNum() == workPostNum
+	 * 
+	 * @throws IllegalArgumentException
+	 * 		| workPostType == null
+	 */
+	public WorkPost(TaskType workPostType, int workPostNum) throws IllegalArgumentException {
+		if (workPostType == null) {
+			throw new IllegalArgumentException("Cannot initialise a work post with a non-existent work post type.");
+		}
+		
+		this.workPostType = workPostType;
+		this.name = workPostType.toString();
+		this.workPostNum = workPostNum;
 	}
 
+	//--------------------------------------------------------------------------
+	// Methods related to WorkPostContainer.
+	//--------------------------------------------------------------------------
+	@Override
+	public String getName() {
+		return this.name;
+	}
+	
+	/** The name of this WorkPost. */
+	private final String name;
+
+	//--------------------------------------------------------------------------
+	@Override
+	public int getWorkPostNum() {
+		return this.workPostNum;
+	}
+	
+	/** The number of this WorkPost. */
+	private int workPostNum;
+
+	//--------------------------------------------------------------------------
+	@Override
+	public TaskType getTaskType() {
+		return this.workPostType;
+	}
+	
+	/** The type of this WorkPost. */
+	private final TaskType workPostType;
+
+	//--------------------------------------------------------------------------
+	@Override
+	public boolean isEmpty() {
+		return this.getAssemblyProcedure() == null;
+	}
+	
+	@Override
+	public List<AssemblyTaskContainer> getMatchingAssemblyTasks() {
+		if (this.isEmpty()) {
+			throw new IllegalStateException();
+		}
+		return this.activeAssembly.getAssemblyTasks(this.getTaskType());
+	}
+
+	@Override
+	public AssemblyProcedureContainer getAssemblyProcedureContainer() {
+		return this.activeAssembly;
+	}
+	
+	/**
+	 * Get the AssemblyProcedure of this WorkPost. 
+	 * 
+	 * @return the ASsemblyProcedure of this WorkPost, null if it does not exist.
+	 */
+	protected AssemblyProcedure getAssemblyProcedure(){
+		return activeAssembly;
+	}
+	
+	/** The assembly procedure the work post is currently working on. */
+	private AssemblyProcedure activeAssembly;
+
+	//--------------------------------------------------------------------------
+	// Methods related to completing a task.
+	//--------------------------------------------------------------------------
+	//TODO new functionality
 	public void completeTask(Object parameter, Object parameter2) {
 		throw new UnsupportedOperationException();
 	}
 
-	public void getActiveAssembly() {
-		throw new UnsupportedOperationException();
-	}
-
+	//TODO new functionality
 	public void incrementTime(Object parameter) {
 		throw new UnsupportedOperationException();
-	}
-
-	public TaskType getWorkPostType() {
-		return this.workPostType;
-	}
-
-	public AssemblyTaskContainer[] toMatchingAssemblyTasksArray() {
-		AssemblyTaskContainer[] lMatchingAssemblyTasks_Temp = new AssemblyTaskContainer[this.matchingAssemblyTasks.size()];
-		this.matchingAssemblyTasks.toArray(lMatchingAssemblyTasks_Temp);
-		return lMatchingAssemblyTasks_Temp;
-	}
+	}	
 }
--- src/domain/WorkPostContainer.java
+++ src/domain/WorkPostContainer.java
@@ -1,8 +1,59 @@
 package domain;
 
+import java.util.List;
+
+/** 
+ * The WorkPostContainer provides an interface that allows external programs 
+ * to access the information of a WorkPost in the domain, without accidental 
+ * (and uncontrolled) state changes. 
+ * It provides methods for accessing its name, workpostnumber, TaskType, and 
+ * AssemblyProcedure.
+ * 
+ * @author Martinus Wilhelmus Tegelaers
+ */
 public interface WorkPostContainer {
-
-	public TaskType getWorkPostType();
-
-	public AssemblyTaskContainer[] toMatchingAssemblyTasksArray();
+	/**
+	 * Get the name of this WorkPostContainer.
+	 * 
+	 * @return The name of this WorkPostContainer.
+	 */
+	public String getName();
+	
+	/**
+	 * Get the number of this WorkPostContainer.
+	 * 
+	 * @return The number of this WorkPostContainer.
+	 */
+	public int getWorkPostNum();
+	
+	/**
+	 * Get the TaskType of this WorkPost.
+	 * 
+	 * @return The TaskType of this WorkPost.
+	 */
+	public TaskType getTaskType();
+	
+	/**
+	 * Get the tasks from the active AssemblyProcedure that match this WorkPost TaskType.
+	 * 
+	 * @return A list of assembly tasks from the assembly procedure with the same type as this WorkPost
+	 * 
+	 * @throws IllegalStateException
+	 * 		| this.isEmpty()
+	 */
+	public List<AssemblyTaskContainer> getMatchingAssemblyTasks();
+	
+	/**
+	 * Check if this WorkPost is currently empty (has no active AssemblyProcedure).
+	 * 
+	 * @return True if no AssemblyProcedure, False otherwise.
+	 */
+	public boolean isEmpty();
+	
+	/**
+	 * Get the active AssemblyProcedureContainer of this WorkPost. 
+	 * 
+	 * @return The active AssemblyProcedureContainer of this WorkPost. 
+	 */
+	public AssemblyProcedureContainer getAssemblyProcedureContainer();
 }
