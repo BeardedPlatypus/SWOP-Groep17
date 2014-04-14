2b4e08b75622e049cad6d2b3f20d1c005986b6f0
BeardedPlatypus <mwtegelaers@gmail.com>
2014-04-14 15:16:31 +0200

    removed som stuff from initHandler, added some stuff, not much progress.

--- src/domain/InitialisationHandler.java
+++ src/domain/InitialisationHandler.java
@@ -1,15 +1,25 @@
 package domain;
 
+//TODO: This needs to be updated since the handlers will not be available anymore to the ui
+/**
+ * The InitialisationHandler is responsible for initialising the system. This
+ * includes making all the appropriate objects and setting their associations. 
+ * It also provides a way for the interface to ask for the appropriate handlers. 
+ * 
+ */
 public class InitialisationHandler {
-	private NewOrderSessionHandler newOrderHandler;
-	private PerformAssemblyTaskHandler taskHandler;
-	public DomainFacade domainFacade;
-
-	public NewOrderSessionHandler getNewOrderHandler() {
-		return this.newOrderHandler;
+	//FIXME: create new constructor
+	
+	/** 
+	 * Get the DomainFacade of the system initialised 
+	 * by this InitialisationHandler.
+	 * 
+	 * @return The DomainFacad of the system initialised by this InitialisationHandler
+	 */
+	public DomainFacade getDomainFacade() {
+		return this.domainFacade;
 	}
-
-	public PerformAssemblyTaskHandler getTaskHandler() {
-		return this.taskHandler;
-	}
+	
+	/** The domain facade that is accessible by the UI. */
+	private final DomainFacade domainFacade;
 }
