2a2cd5b4410cbed1b60a0ab708d01031d1b9a721
BeardedPlatypus <mwtegelaers@gmail.com>
2014-04-15 00:16:24 +0200

    updated tasktype (copypasted, no new stuff)

--- src/domain/TaskType.java
+++ src/domain/TaskType.java
@@ -4,5 +4,28 @@ package domain;
  * A representation of all possible types of work posts.
  */
 public enum TaskType {
-	BODY("Body Post"), DRIVETRAIN("Drivetrain Post"), ACCESSORIES("Accessories Post");
+	BODY("Body Post"),
+	DRIVETRAIN("Drivetrain Post"),
+	ACCESSORIES("Accessories Post");
+	
+	private String name;
+	
+	/**
+	 * Construct a new TaskType with the specified name.
+	 * 
+	 * @param name
+	 * 		The name of the new TaskType.
+	 */
+	TaskType(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Get the name of this TaskType.
+	 * 
+	 * @return the name of this TaskTyp.
+	 */
+	public String getName() {
+		return this.name;
+	}
 }
