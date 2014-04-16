7cd7f68432da44163d0c911bfd0660adb08ad77b
Thomas Vochten <thomas.vochten@student.kuleuven.be>
2014-04-16 17:25:44 +0200

    Some tests concerning the production statistics were accidentally not included.

---
+++ test/domain/DelayRegistrarTest.java
@@ -0,0 +1,110 @@
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.javatuples.Pair;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+public class DelayRegistrarTest {
+	
+	@Rule public ExpectedException exception = ExpectedException.none();
+	
+	DelayRegistrar registrar;
+	double epsilon = 1E-14;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		registrar = new DelayRegistrar();
+		
+		ProcedureStatistics stats = new ProcedureStatistics(10);
+		registrar.addStatistics(stats);
+		
+		stats = new ProcedureStatistics(20);
+		registrar.addStatistics(stats);
+		
+		stats = new ProcedureStatistics(50);
+		registrar.addStatistics(stats);
+		
+		stats = new ProcedureStatistics(30);
+		registrar.addStatistics(stats);
+		
+		stats = new ProcedureStatistics(40);
+		registrar.addStatistics(stats);
+		
+		stats = new ProcedureStatistics(10);
+		registrar.addStatistics(stats);
+	}
+	
+	public void addStatisticsTest() {
+		ProcedureStatistics stats = new ProcedureStatistics(1000);
+		int dayNumber = registrar.getActiveDay().getDayNumber();
+		registrar.addStatistics(stats);
+		Pair<Integer, WorkingDay> lastDelay = registrar.getDelays().get(registrar.getDelays().size());
+		assertTrue(1000 == lastDelay.getValue0());
+		assertEquals(dayNumber, registrar.getDelays().get(registrar.getDelays().size() - 1).getValue1().getDayNumber());
+	}
+
+	@Test
+	public void switchDayTest() {
+		registrar.switchDay(1);
+		assertEquals(1, registrar.getActiveDay().getDayNumber());
+	}
+	
+	@Test
+	public void switchDayTest_equalDayNumber() {
+		registrar.switchDay(0);
+		assertEquals(0, registrar.getActiveDay().getDayNumber());
+	}
+	
+	@Test
+	public void switchDayTest_smallerDayNumber() {
+		registrar.switchDay(1);
+		exception.expect(IllegalArgumentException.class);
+		registrar.switchDay(0);
+	}
+	
+	@Test
+	public void getAverageTest() {
+		double average = registrar.getAverage();
+		assertEquals(80d/3d, average, epsilon);
+	}
+	
+	@Test
+	public void getAverageTest_noStats() {
+		DelayRegistrar registrar = new DelayRegistrar();
+		assertEquals(0, registrar.getAverage(), epsilon);
+	}
+	
+	@Test
+	public void getMedianTest() {
+		double median = registrar.getMedian();
+		assertEquals(25, median, epsilon);
+	}
+	
+	@Test
+	public void getMedianTest_noStats() {
+		DelayRegistrar registrar = new DelayRegistrar();
+		assertEquals(0, registrar.getMedian(), epsilon);
+	}
+	
+	@Test
+	public void getMedianTest_oneStat() {
+		DelayRegistrar registrar = new DelayRegistrar();
+		registrar.addStatistics(new ProcedureStatistics(100));
+		assertEquals(100, registrar.getMedian(), epsilon);
+	}
+	
+	@Test
+	public void getStatsTest() {
+		System.out.println(registrar.getStatistics());
+	}
+
+}
---
+++ test/domain/StatisticsLoggerTest.java
@@ -0,0 +1,67 @@
+package domain;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+public class StatisticsLoggerTest {
+	
+	StatisticsLogger logger = new StatisticsLogger();
+	
+	@Mock DelayRegistrar delayRegistrar;
+	@Mock CarsProducedRegistrar carsRegistrar;
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		
+		logger.addRegistrar(delayRegistrar);
+		logger.addRegistrar(carsRegistrar);
+		
+		Mockito.when(delayRegistrar.isValidDay(1)).thenReturn(true);
+		Mockito.when(carsRegistrar.isValidDay(1)).thenReturn(true);
+		Mockito.when(delayRegistrar.isValidDay(-1)).thenReturn(false);
+		Mockito.when(carsRegistrar.isValidDay(-1)).thenReturn(false);
+	}
+
+	@Test
+	public void addStatisticsTest() {
+		ProcedureStatistics stats = new ProcedureStatistics(100);
+		logger.addStatistics(stats);
+		Mockito.verify(delayRegistrar).addStatistics(stats);
+		Mockito.verify(carsRegistrar).addStatistics(stats);
+	}
+	
+	@Test
+	public void getReportTest() {
+		logger.getReport();
+		Mockito.verify(delayRegistrar).getStatistics();
+		Mockito.verify(carsRegistrar).getStatistics();
+	}
+	
+	@Test
+	public void updateTimeTest_validDay() {
+		logger.update(new DateTime(1, 0, 0));
+		Mockito.verify(delayRegistrar).isValidDay(1);
+		Mockito.verify(carsRegistrar).isValidDay(1);
+		Mockito.verify(delayRegistrar).switchDay(1);
+		Mockito.verify(carsRegistrar).switchDay(1);
+	}
+	
+	@Test
+	public void updateTimeTest_invalidDay() {
+		logger.update(new DateTime(-1, 0, 0));
+		Mockito.verify(delayRegistrar, Mockito.never()).switchDay(-1);
+		Mockito.verify(carsRegistrar, Mockito.never()).switchDay(-1);
+	}
+
+}
