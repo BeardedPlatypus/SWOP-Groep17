4d612266d5e4175d38a0fcd516f19afd75e626b3
Frederik Goovaerts <goovaertsfrederik@hotmail.com>
2014-04-20 20:10:23 +0200

    Updated Specification Tests

--- test/domain/SpecificationTest.java
+++ test/domain/SpecificationTest.java
@@ -3,22 +3,26 @@ package domain;
 import static org.junit.Assert.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class SpecificationTest {
+	@Rule public ExpectedException exception = ExpectedException.none();
 
 	Specification testSpec;
 	Option option1;
-	Option option1;
-	Option option1;
+	Option option2;
+	Option option3;
 	
 	@Before
 	public void setUp() throws Exception {
-		Option option1 = new Option(TaskType.BODY, "body", "mount body");
-		Option option2 = new Option(TaskType.DRIVETRAIN, "drivetrain", "mount drivetrain");
-		Option option3 = new Option(TaskType.ACCESSORIES, "accessories", "mount accessories");
+		option1 = new Option(TaskType.BODY, "body", "mount body");
+		option2 = new Option(TaskType.DRIVETRAIN, "drivetrain", "mount drivetrain");
+		option3 = new Option(TaskType.ACCESSORIES, "accessories", "mount accessories");
 		ArrayList<Option> options = new ArrayList<>();
 		options.add(option1);
 		options.add(option2);
@@ -30,22 +34,54 @@ public class SpecificationTest {
 	public void constructorTest() {
 		assertTrue(testSpec.getAmountOfOptions() == 3);
 	}
+
+	@Test
+	public void constructorTestNullList() {
+		exception.expect(IllegalArgumentException.class);
+		testSpec = new Specification(null);
+	}
+
+	@Test
+	public void constructorTestNullInList() {
+		exception.expect(IllegalArgumentException.class);
+		List<Option> opts = new ArrayList<>();
+		opts.add(option1);
+		opts.add(null);
+		testSpec = new Specification(opts);
+	}
 	
 	@Test
 	public void getSpecTest() {
-		assertTrue(testSpec.getOption(0).equals(obj));
-		assertTrue(testSpec.getSpec(1) == 4);
-		assertTrue(testSpec.getSpec(2) == 3);
+		assertTrue(testSpec.getOption(0).equals(option1));
+		assertTrue(testSpec.getOption(1).equals(option2));
+		assertTrue(testSpec.getOption(2).equals(option3));
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void getSpecTestTooBigException() {
-		testSpec.getSpec(4);
+		testSpec.getOption(4);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void getSpecTestNegativeException() {
-		testSpec.getSpec(-1);
+		testSpec.getOption(-1);
+	}
+
+	@Test
+	public void testEqualsObject() {
+		assertFalse(testSpec.equals(null));
+		assertFalse(testSpec.equals(option1));
+		List<Option> sameOpts = new ArrayList<>();
+		sameOpts.add(option1);
+		sameOpts.add(option2);
+		sameOpts.add(option3);
+		List<Option> diffOpts = new ArrayList<>();
+		diffOpts.add(option1);
+		Specification testSpecSame = new Specification(sameOpts);
+		Specification testSpecDiff = new Specification(diffOpts);
+		assertTrue(testSpec.equals(testSpecSame));
+		assertFalse(testSpec.equals(testSpecDiff));
+		assertTrue(testSpec.equals(testSpec));
 	}
 
 }
