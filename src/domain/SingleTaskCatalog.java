9693f28976a798f20c165042279420e369cb830e
SimSla <simon@Simons-MacBook-Air.local>
2014-04-21 13:54:33 +0200

    single task order

--- src/domain/SingleTaskCatalog.java
+++ src/domain/SingleTaskCatalog.java
@@ -1,13 +1,35 @@
 package domain;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import util.annotations.Immutable;
 import domain.OptionCategory;
 
-//TODO new in iteration 2
+/**
+ * Provides an overview of the tasks that can be ordered
+ * individually by a customs shop.
+ * 
+ * @author Simon Slangen
+ */
+@Immutable
 public class SingleTaskCatalog {
+	
 	private ArrayList<OptionCategory> optionCategories = new ArrayList<OptionCategory>();
+	
+	/**
+	 * Initializes the single task catalog with the given list of option categories.
+	 * 
+	 * @post getPossibleTasks() is not null
+	 */
+	public SingleTaskCatalog(List<OptionCategory> inputOptionCategories){
+		this.optionCategories = new ArrayList<OptionCategory>(inputOptionCategories);
+	}
 
-	public void getPossibleTasks() {
-		throw new UnsupportedOperationException();
+	/**
+	 * Returns a list of option categories in this single task catalog.
+	 */
+	public ArrayList<OptionCategory> getPossibleTasks() {
+		return new ArrayList<OptionCategory>(this.optionCategories);
 	}
 }
