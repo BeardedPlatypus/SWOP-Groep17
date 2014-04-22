384542bd2e7d6b62e59839804dff95eeccf38c28
BeardedPlatypus <mwtegelaers@gmail.com>
2014-04-22 02:14:23 +0200

    Added documentation to ProductionScheduleFacade.

--- src/domain/productionSchedule/ProductionScheduleFacade.java
+++ src/domain/productionSchedule/ProductionScheduleFacade.java
@@ -1,27 +1,83 @@
 package domain.productionSchedule;
 
+import java.util.List;
+
+import domain.DateTime;
 import domain.Manufacturer;
 import domain.Model;
 import domain.Option;
-import domain.ProductionSchedule;
 import domain.order.OrderContainer;
 
+/** 
+ * The ProductionScheduleFacade provides an interface to the ProductionSchedule
+ * subsystem. It provides the basic methods the rest of the domain can use to 
+ * interact with this ProductionSchedule.
+ * 
+ * @author Martinus Wilhelmus Tegelaers
+ *
+ */
 public class ProductionScheduleFacade {
+	//--------------------------------------------------------------------------
+	// Manufacturer
+	//--------------------------------------------------------------------------
+	//TODO initialisation stuff: set manufacturer properly
+	/** 
+	 * Get the Manufacturer that owns this ProductionScheduleFacade. 
+	 * 
+	 * @return The Manufacturer that owns this ProductionScheduleFacade.
+	 */
+	private Manufacturer getManufacturer() {
+		return this.manufacturer;
+	}
+	
+	/** The manufacturer that owns this ProductionScheduleFacade. */
 	private Manufacturer manufacturer; 
 	
-	public void getPendingOrdersContainers() {
+	//--------------------------------------------------------------------------
+	// Get methods.
+	//--------------------------------------------------------------------------
+	/**
+	 * Get the pending orders of this ProductionSchedule. 
+	 * 
+	 * @return The pending orders of this ProductionSchedule.
+	 * 	       | ALL o: o == OrderContainer && o in this 
+	 */
+	public List<OrderContainer> getPendingOrdersContainers() {
 		throw new UnsupportedOperationException();
 	}
 
+	
 	public void submitStandardOrder(Model model, Option options) {
 		throw new UnsupportedOperationException();
 	}
 
-	public void getEstimatedCompletionTime(Object parameter) {
+	/** 
+	 * Get the estimated completion time of the specified order in this ProductionSchedule.
+	 * 
+	 * @param order
+	 * 		The specified order of which the estimated completion time is requested.
+	 * 
+	 * @return The estimated completion time of the specified order.
+	 * 
+	 * @throws IllegalArgumentException
+	 * 		| order == null || !this.contains(order)
+	 */
+	public DateTime getEstimatedCompletionTime(OrderContainer order) throws IllegalArgumentException {
 		throw new UnsupportedOperationException();
 	}
 
-	public boolean contains(OrderContainer order) {
+	/**
+	 * Check if the specified order is an element of this ProductionSchedule.
+	 * 
+	 * @param order
+	 * 		The Specified order that should be checked.
+	 * 
+	 * @return | order in this
+	 * 
+	 * @throws IllegalArgumentException
+	 * 		| order == null
+	 */
+	public boolean contains(OrderContainer order) throws IllegalArgumentException {
 		throw new UnsupportedOperationException();
 	}
 }
