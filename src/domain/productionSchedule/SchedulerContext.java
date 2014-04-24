75f6226fe8c61df8f2f0867b09827e65d93bef09
BeardedPlatypus <mwtegelaers@gmail.com>
2014-04-24 12:27:18 +0200

    moar functions

--- src/domain/productionSchedule/SchedulerContext.java
+++ src/domain/productionSchedule/SchedulerContext.java
@@ -171,6 +171,59 @@ public class SchedulerContext {
 	}
 	
 	/**
+	 * Pop the next SingleTaskOrder to be scheduled of the specified TaskType from
+	 * its respective queue.
+	 * If the scheduling algorithm is done after popping, it will be set to the 
+	 * default algorithm.
+	 * 
+	 * @param t
+	 * 		The TaskType of the SingleTaskOrder to be popped. 
+	 * 
+	 * @return the next SingleTaskOrder to be scheduled of the specified type
+	 * 
+	 * @postcondition | !(new this).contains(result)
+	 * @postcondition | !E (o) o.deadline && == o.TaskType == t < result.deadline
+	 * @postcondition | currentSchedulingAlgorithm.isDone() -> (new this).getCurrentSchedulingAlgorithm() == this.getDefaultAlgorithm
+	 * 
+	 * @throws IllegalStateException
+	 * 		| !this.hasSingleTaskOrdersOfType(t)
+	 */
+	StandardOrder popNextStandardOrder() throws IllegalStateException {
+		if (!this.hasStandardOrders())
+			throw new IllegalStateException();
+		StandardOrder result = this.getOrderQueueRaw().remove(0);
+
+		if (this.getCurrentSchedulingStrategy().isDone(this.getOrderQueueRaw()))
+			this.setSchedulingStrategy(this.getDefaultStrategy());
+		return result;
+	}
+
+	
+	/**
+	 * Get the next StandardOrder to be scheduled (does not modify the queue).
+	 * 
+	 * @return the next StandardOrder to be scheduled.
+	 * 
+	 * @throws IllegalStateException
+	 * 		| !this.hasStandardOrders()
+	 */
+	public StandardOrder getNextStandardOrder() {
+		if (!this.hasStandardOrders())
+			throw new IllegalStateException();
+		
+		return this.getOrderQueue().get(0);
+	}
+	
+	/**
+	 * Check if the StandardOrders queue is not empty.
+	 *  
+	 * @return true if queue not empty, else false.
+     */
+	boolean hasStandardOrders() {
+		return !this.getOrderQueue().isEmpty();
+	}
+	
+	/**
 	 * Return the orderQueue of this SchedulerContext.
 	 * 
 	 * @return the orderQueue of this SchedulerContext
@@ -217,8 +270,15 @@ public class SchedulerContext {
 	 * 		The TaskType of the next SingleTaskOrder to be scheduled.
 	 * 
 	 * @return The next SingleTaskOrder of the specified TaskType to be scheduled.
+	 * 
+	 * @throws IllegalStateException
+	 * 		| !this.hasSingleTaskOrdersOfType(t)
+
 	 */
-	protected SingleTaskOrder getNextSingleTaskOrderOfType(TaskType t) {
+	SingleTaskOrder getNextSingleTaskOrderOfType(TaskType t) {
+		if (!this.hasSingleTaskOrdersOfType(t))
+			throw new IllegalStateException();
+		
 		return this.getSingleTaskOrdersOfType(t).get(0);
 	}
 	
@@ -233,9 +293,27 @@ public class SchedulerContext {
 	 * 
 	 * @postcondition | !(new this).contains(result)
 	 * @postcondition | !E (o) o.deadline && == o.TaskType == t < result.deadline
+	 * 
+	 * @throws IllegalStateException
+	 * 		| !this.hasSingleTaskOrdersOfType(t)
 	 */
-	protected SingleTaskOrder popNextSingleTaskOrderOfType(TaskType t) {
+	SingleTaskOrder popNextSingleTaskOrderOfType(TaskType t) throws IllegalStateException {
+		if (!this.hasSingleTaskOrdersOfType(t))
+			throw new IllegalStateException();
+
 		return this.taskOrderQueue.get(t).remove(0);
+	}
+	
+	/**
+	 * check if the queue of the specified Tasktype SingleTaskOrders is not empty.
+	 * 
+	 * @param t
+	 * 		The TaskType of the SingleTaskOrder queue that h
+	 * 
+	 * @return true if queue not empty, else false.
+	 */
+	boolean hasSingleTaskOrdersOfType(TaskType t) {
+		return !this.getSingleTaskOrdersOfType(t).isEmpty();
 	}
 	
 	/**
