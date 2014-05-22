81923053cbf317c591da0b550b17c6e384bd82d4
Thomas Vochten <thomas.vochten@student.kuleuven.be>
2014-05-22 16:37:18 +0200

    Merge conflict en al

--- test/scenario/CheckAssemblyLineStatusScenario.java
+++ test/scenario/CheckAssemblyLineStatusScenario.java
@@ -10,25 +10,13 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.powermock.api.mockito.PowerMockito;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.powermock.reflect.Whitebox;
 
-import domain.Manufacturer;
-import domain.assembly_line.AssemblyLine;
-import domain.assembly_line.AssemblyProcedure;
-import domain.assembly_line.AssemblyTask;
-import domain.assembly_line.AssemblyTaskView;
-import domain.assembly_line.TaskType;
-import domain.assembly_line.WorkPost;
-import domain.assembly_line.WorkPostView;
-import domain.car.Option;
+import domain.assembly_line.AssemblyLineView;
 import domain.handlers.AssemblyLineStatusHandler;
 import domain.handlers.DomainFacade;
 import domain.handlers.InitialisationHandler;
-import domain.order.Order;
 
 @RunWith(PowerMockRunner.class)
 public class CheckAssemblyLineStatusScenario {
@@ -58,25 +46,12 @@ public class CheckAssemblyLineStatusScenario {
 		// including pending and finished tasks at each work post.
 		// --- Only getting the information is tested, formatting is the responsibility 
 		// of the UI ---
-		assertEquals(3, handler.getAmountOfWorkPosts());
-		List<AssemblyTaskView> tasks;
-		List<WorkPostView> containers = handler.getWorkPosts();
-		assertEquals(handler.getWorkPost(0), containers.get(0));
-		assertFalse(workPosts.get(0).isFinished());
-		assertTrue(workPosts.get(1).isEmpty());
-		assertFalse(workPosts.get(2).isFinished());
-		assertEquals(2, handler.getAmountOfTasksAtWorkPost(0));
-		tasks = handler.getTasksAtWorkPost(0);
-		assertEquals(tasks.get(0), task1);
-		assertEquals(tasks.get(1), task2);
-		assertTrue(tasks.get(0).isCompleted());
-		assertFalse(tasks.get(1).isCompleted());
-		
-		tasks = handler.getTasksAtWorkPost(2);
-		assertEquals(tasks.get(0), task3);
-		assertEquals(tasks.get(1), task4);
-		assertTrue(tasks.get(0).isCompleted());
-		assertFalse(tasks.get(1).isCompleted());
+		AssemblyLineView view = handler.getLineViews().get(0);
+		assertEquals(3, view.getWorkPostViews().size());
+		assertFalse(view.getWorkPostViews().get(0).isEmpty());
+		assertTrue(view.getWorkPostViews().get(1).isEmpty());
+		assertTrue(view.getWorkPostViews().get(2).isEmpty());
+		assertFalse(view.getWorkPostViews().get(0).isFinished());
 	}
 
 }
